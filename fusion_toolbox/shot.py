78709a421e9cfb62326f42fae3e71681b182e960
andrew-maris <maris@mit.edu>
2023-09-21 13:02:58 -0400

    Updating based on first round of feedback

--- fusion_toolbox/shot.py
+++ fusion_toolbox/shot.py
@@ -2,26 +2,45 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 class Shot:
-    """Shot class to store data from a shot."""
-    def __init__(self, path: str, signals:list = []):
-        self.path = path
-        self.signals = signals
+    """Shot class to store data from a shot.
+    
+    Parameters
+    ----------
+    path : string
+        Path to the shot data file.
+
+    """
+    def __init__(self, path: str=None):
+        self.path = path # Path to the shot data file
         self.data = {} # Dictionary of signals. Each signal is a dictionary with keys "time" and "value"
         self.tokamak = None
         self.shot_id = None
         self.events = {}
-        
-        #For now, default to loading from csv file
-        self.load_csv()
-        
 
-    def load_csv(self):
+        # Load data from csv if path is given
+        if path is not None:
+            self.from_csv(path)
+        
+    @classmethod
+    def from_csv(path: str, self):
         """Loads the data of a shot from the database into shot object.
+
+        Parameters
+        ----------
+        path : string
+            Path to the shot data file.
+
+        Example
+        --------
+        >>> my_shot = Shot()
+        >>> my_shot.from_csv("shots/shot_2023010101.csv")
 
         """
         
-        df = pd.read_csv(self.path)
+        df = pd.read_csv(path)
+        self.path = path
         if 'time' not in df.columns:
             print('Warning: no time column found in shot data. Creating dummy time column.')
             df["time"] = np.arange(len(df))
@@ -49,9 +68,57 @@ class Shot:
 
 
 def plot_shot(shot: Shot, signal: str, **kwargs):
+    """Plots a signal from a shot object.
+
+    Parameters
+    ----------
+    shot : Shot
+        Shot object.
+    signal : str
+        Name of the signal to plot.
+    **kwargs : dict
+        Keyword arguments to pass to matplotlib.pyplot.plot.
+
+    Returns
+    -------
+    matplotlib.plot object
+
+    Example
+    -------
+    >>> my_shot = Shot(path="shots/shot_2023010101.csv")
+    >>> fig, axs = plt.subplots(ncols=1, nrows=2, sharex=True)
+    >>> plt.sca(axs[0])
+    >>> axs[0].set_ylim([0,2000])
+    >>> plot_shot(my_shot, "Plasma_Current (A)", color="tab:red")
+    >>> plt.sca(axs[1])
+    >>> plot_shot(my_shot, "Plasma_Density (particlesm3)")
+    >>> plt.show()
+
+
+    """
     return plt.plot(shot.data[signal]["time"], shot.data[signal]["value"], **kwargs)
 
-def plot_standard_shot(shots):
+def plot_standard_shot(shots: list):
+    """Plots a standard set of signals from a list of shots.
+
+    Parameters
+    ----------
+    shots : list
+        List of Shot objects.
+
+    Returns
+    -------
+    fig, axs : matplotlib.pyplot.subplots object
+
+    Example
+    -------
+    >>> my_shot = Shot(path="shots/shot_2023010101.csv")
+    >>> my_shot2 = Shot(path="shots/shot_2023010102.csv")
+    >>> fig, axs = plot_standard_shot([my_shot,my_shot2])
+    >>> plt.show()
+
+    """
+
     fig, axs = plt.subplots(ncols=1, nrows=2, sharex=True)
     for shot in shots:
         plt.sca(axs[0])
@@ -62,22 +129,3 @@ def plot_standard_shot(shots):
 
     return fig, axs
 
-if __name__ == "__main__":
-
-    print("Example shot.py")
-    my_shot = Shot(path="shots/shot_2023010101.csv", signals=[])
-
-    # Plot using matplotlib directly
-    fig, axs = plt.subplots(ncols=1, nrows=2, sharex=True)
-    plt.sca(axs[0])
-    axs[0].set_ylim([0,2000])
-    plot_shot(my_shot, "Plasma_Current (A)", color="tab:red")
-    plt.sca(axs[1])
-    plot_shot(my_shot, "Plasma_Density (particlesm3)")
-    plt.show()
-
-
-    # Plot using the plot_standard_shot function
-    my_shot2 = Shot(path="shots/shot_2023010102.csv", signals=[])
-    fig, axs = plot_standard_shot([my_shot,my_shot2])
-    plt.show()
