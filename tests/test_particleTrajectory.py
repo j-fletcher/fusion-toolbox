3dc425fd757652e915b28579060769a783a045b9
adevitre <42851029+adevitre@users.noreply.github.com>
2023-09-21 14:08:14 -0400

    resolving conflict with shon

--- tests/test_particleTrajectory.py
+++ tests/test_particleTrajectory.py
@@ -2,7 +2,6 @@ import numpy as np
 import scipy.constants as constants
 import numpy.testing as npt
 import pytest
-
 @pytest.mark.parametrize(
     "test, expected",
     [
@@ -17,24 +16,3 @@ def test_updatePosition(test, expected):
     particle = ChargedParticle(initialPosition=test[0], initialVelocity=test[1], charge=constants.elementary_charge, mass=constants.proton_mass)
     particle.updatePosition(test[2])
     npt.assert_array_equal(particle.getPosition(), np.array(expected))
-
-
-@pytest.mark.parametrize(
-    "test, expected",
-    [
-        ([0.5, 0, 0],[3, 0, 0]),
-        ([.5,.5,.5],[4.5,0,0])
-    ]
-)
-def test_fieldInterpolation(test, expected):
-    '''Tests the field interpolation routine'''
-    import fusion_toolbox.particle_trajectory.Field as F
-    x=np.linspace(0,1,2)
-    y=np.linspace(0,1,2)
-    z=np.linspace(0,1,2)
-    Bx=np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
-    By=np.zeros((2,2,2))
-    Bz=np.zeros((2,2,2))
-    B = F.Field(x, y, z, Bx, By, Bz)
-    interpB = B.interpolateField(test)
-    npt.assert_equal(interpB, expected)
